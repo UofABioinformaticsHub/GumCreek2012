145eaaf7dab6b7e881c7760dc2a6c7ca0e108250
Steve Pederson <stephen.pederson@adelaide.edu.au>
2017-09-23 02:53:40 +0930

    Finished Filtering SNPs

--- scripts/01_StacksPipeline.sh
+++ scripts/01_StacksPipeline.sh
@@ -5,6 +5,8 @@
 # Renaming after the stacks pair-renaming crap
 
 # Aligning
+# The masked genome was obtained from NCBI:
+# ftp://ftp.ncbi.nih.gov/genomes/Oryctolagus_cuniculus/Assembled_chromosomes/seq/
 
 # ref_map
 
